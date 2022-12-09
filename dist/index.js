0b3dff844482f944d504fa89976cfdb0a04e0394
gougisle <lefebagougis@gmail.com>
2022-12-08 21:15:33 -0800

    adding out github action

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+//This is JS scripte we will run. This is our Action script
+async function run() {
+  console.log("Hello World");
+}
+
+run();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
---
+++ src/action.js
@@ -0,0 +1,6 @@
+//This is JS scripte we will run. This is our Action script
+async function run() {
+  console.log("Hello World");
+}
+
+run();
